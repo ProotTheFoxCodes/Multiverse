9f8cb8561618a65c6ce5f240b5fe8c6932453e28
Alvin Dong <alvindong9913@gmail.com>
2025-08-01 13:05:34 -0500

    Added new Jokers
    addition: Added Summoned Skull, 50/50, and Victory Royale
    
    fixes: Removed unnecessary context check from Antimatter
    Changed mul_transmutable application for Joker and Villager to use card:add_sticker
    Changed Holy Grail to use the event queue to create cards
    Went through every call of SMODS.create_card to put key_append = mod_key .. source
    
    balance: Made V1 able to be blueprinted

--- mod/overrides.lua
+++ mod/overrides.lua
@@ -1,6 +1,6 @@
 SMODS.Joker:take_ownership("joker", {
     transmutable_compat = true,
-    config = {extra = {mult = 4, tarots_used = {n = 0}, transmute_req = 15}}, -- Original value is 15
+    config = {extra = {mult = 4, tarots_used = {n = 0}, transmute_req = 1}}, -- Original value is 15
     loc_vars = function(self, info, card)
         table.insert(info, {
             set = "Other", key = "mul_joker_hint"
@@ -18,7 +18,8 @@ SMODS.Joker:take_ownership("joker", {
             end
             if card.ability.extra.tarots_used.n >= card.ability.extra.transmute_req then
                 -- note to self: when adding modded stickers, must add mod prefix before sticker key
-                card.ability.mul_transmutable = true
+                print("entered")
+                card:add_sticker("mul_transmutable", true) -- another note to self: pass in true as 2nd argument
             end
         end
     end
