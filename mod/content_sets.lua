4a2d5649729de877f43c8ed78786d0ff49a980af
Alvin Dong <alvindong9913@gmail.com>
2025-09-10 18:23:04 -0500

    Update content_sets.lua
    added The Burger to joke card set

--- mod/content_sets.lua
+++ mod/content_sets.lua
@@ -2,7 +2,8 @@ SMODS.ObjectType {
     key = "mul_joke",
     default = "c_mul_apple",
     cards = {
-        c_mul_apple = true
+        c_mul_apple = true,
+        c_mul_burger = true
     }
 }
 SMODS.ObjectType {
